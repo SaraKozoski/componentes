1f6c544484b1a7f3521216c9ab307fa27137f0cf
SaraKozoski <sarakozoski@gmail.com>
2024-07-12 15:06:49 -0300

    atiidade dev web componentes

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import ExpandBox from './components/ExpandBox.vue';
+</script>
+
+<template>
+  <div>
+    <expand-box title="Título da caixa de expansão" />
+  </div>
+  <div>
+    <expand-box title="Título da caixa de exp" />
+  </div>
+  <div>
+    <expand-box title="Título da caixa de expans" />
+  </div>
+  <div>
+    <expand-box title="Título da caixa de expan" />
+  </div>
+</template>
